0fd45cf324b95c913d02eea748f3195df452f9b7
Brian Huamani <bhuamanih@proveedor.intercorp.com.pe>
2026-07-05 14:41:57 -0500

    Initial Commit

---
+++ UserManager/UserManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagerApp.swift
+//  UserManager
+//
+//  Created by Brian Huamani on 5/07/26.
+//
+
+import SwiftUI
+
+@main
+struct UserManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
